300bc4e3e1358a8bebd43779981fb7b89160fa66
lnannettik <l.nannetti.k@gmail.com>
2022-01-26 17:09:08 +0100

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.main')
+
+@section('content')
+<section class="container">
+    <h1>Welcome to Comics</h1>
+
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea eligendi nobis ipsa veniam temporibus, porro asperiores explicabo, cumque consequuntur minus exercitationem ipsum quis. Alias, cumque! Facere quibusdam officia vero laudantium.
+    </p>
+
+</section>
+    
+@endsection
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="text-center mt-5">
+
+    Comics - for comics lover
+    
+</footer>
